09382c4fa8fb87cfa3c7e72ed72e9209f03c3eaa
Dennis Worsham <dennis@worshamweb.com>
2025-04-20 17:01:20 -0700

    Better comments

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,6 +1,6 @@
 class Forecast < ApplicationRecord
-  attr_accessor :from_cache
-  attribute :forecast, :json
+  attr_accessor :from_cache #this is the flag to indicate if the record is fetched from cache or not
+  attribute :forecast, :json # Simplifies storing/working with JSON data in SQLite3
 
   validates :zip_code, presence: true, uniqueness: true
   validates :forecast, presence: true
@@ -10,10 +10,10 @@ class Forecast < ApplicationRecord
     cached_value = Rails.cache.read(cache_key)
     cached_value.from_cache = true if cached_value.present?
 
+    # If the record exists in cache, return from there. Otherwise fetch from API and cache it.
     if cached_value
       cached_value
     else
-      from_cache = false
       weather_api_data = WeatherApiService.fetch(zip_code)
       new_record = find_or_create_by(zip_code: zip_code, forecast: weather_api_data)
       new_record.from_cache = false
